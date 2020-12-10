ac7561223209617e2f0a08821a64627ef5ee0f2a
Ivo Branco <ivo.branco@fccn.pt>
2020-12-10 16:30:59 +0000

    Remove unnecessary files.

--- update-site-url.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-MAINTENANCE_URL=$1
-echo "MAINTENANCE_URL: $MAINTENANCE_URL"
-rm -rf target
-cp -r site target
-
-find target -type f -exec sed -i "s|{{MAINTENANCE_URL}}|${MAINTENANCE_URL}|g" {} \;
