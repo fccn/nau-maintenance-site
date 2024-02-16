7706cb045a7b12d5f946524f61398eb8d95d9b68
Ivo Branco <ivo.branco@fccn.pt>
2024-02-16 11:58:48 +0000

    fix: acl-public on deploy

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ declare -a environments=("nau-dev-shared" "nau-stage-shared" "nau-prod-shared")
 
 for environment in "${environments[@]}"
 do
-  AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} s3cmd --host rgw.nau.fccn.pt --host-bucket $environment put maintenance-site/index.html s3://$environment/maintenance-site/index.html
+  AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} s3cmd --host rgw.nau.fccn.pt --host-bucket $environment put maintenance-site/index.html s3://$environment/maintenance-site/index.html --acl-public
 done
